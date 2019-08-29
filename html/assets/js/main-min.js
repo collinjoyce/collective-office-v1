1339053b2f9590eff4251aefffc9b8b0369ae54b
Phil Zelnar <zelnar@wamu.org>
2019-08-28 22:46:06 -0600

    Building global masthead and footer

--- html/assets/js/main-min.js
+++ html/assets/js/main-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){});
+$(document).ready(function(){$("#overlay-toggle").click(function(){return $("body").toggleClass("has-overlay"),!1}),$(".masthead").click(function(a){$(a.target).is("a")||$("body").removeClass("has-overlay")})});
--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
     
+    
+    // Nav Toggle
+    $('#overlay-toggle').click(function(){
+        $('body').toggleClass('has-overlay');
+        return false;
+    });
+    // …so clicking something that's not a link closes the nav
+    function handler(event) {
+        var target = $( event.target );
+        if (target.is('a')) {
+        } else {
+            $('body').removeClass('has-overlay');
+        }
+    }
+    $('.masthead').click(handler);
+    
         
 });
