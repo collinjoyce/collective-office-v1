6dcaa26d1f3a6332ce3f3bb33b27bd0854e42408
Phil Zelnar <zelnar@wamu.org>
2019-09-18 19:54:35 -0600

    Updating CP trigger

--- config/general.php
+++ config/general.php
@@ -18,7 +18,7 @@ return [
         'omitScriptNameInUrls' => true,
 
         // Control Panel trigger word
-        'cpTrigger' => 'admin',
+        'cpTrigger' => 'craft',
 
         // The secure key Craft will use for hashing and encrypting data
         'securityKey' => getenv('SECURITY_KEY'),
