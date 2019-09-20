dad39498d484b6192268e07199fbc2268ed616cc
Phil Zelnar <phil@philzelnar.com>
2019-09-20 11:38:47 -0700

    Adding missing /vendor files

---
+++ vendor/craftcms/plugins.php
@@ -0,0 +1,57 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+$rootDir = dirname(dirname(__DIR__));
+
+return array (
+  'sebastianlenz/linkfield' => 
+  array (
+    'class' => 'typedlinkfield\\Plugin',
+    'basePath' => $vendorDir . '/sebastianlenz/linkfield/src',
+    'handle' => 'typedlinkfield',
+    'aliases' => 
+    array (
+      '@typedlinkfield' => $vendorDir . '/sebastianlenz/linkfield/src',
+    ),
+    'name' => 'Typed link field',
+    'version' => '1.0.19',
+    'description' => 'A Craft field type for selecting links',
+    'developer' => 'Sebastian Lenz',
+    'developerUrl' => 'https://github.com/sebastian-lenz/',
+  ),
+  'craftcms/redactor' => 
+  array (
+    'class' => 'craft\\redactor\\Plugin',
+    'basePath' => $vendorDir . '/craftcms/redactor/src',
+    'handle' => 'redactor',
+    'aliases' => 
+    array (
+      '@craft/redactor' => $vendorDir . '/craftcms/redactor/src',
+    ),
+    'name' => 'Redactor',
+    'version' => '2.4.0',
+    'description' => 'Edit rich text content in Craft CMS using Redactor by Imperavi.',
+    'developer' => 'Pixel & Tonic',
+    'developerUrl' => 'https://pixelandtonic.com/',
+    'developerEmail' => 'support@craftcms.com',
+    'documentationUrl' => 'https://github.com/craftcms/redactor/blob/v2/README.md',
+  ),
+  'verbb/super-table' => 
+  array (
+    'class' => 'verbb\\supertable\\SuperTable',
+    'basePath' => $vendorDir . '/verbb/super-table/src',
+    'handle' => 'super-table',
+    'aliases' => 
+    array (
+      '@verbb/supertable' => $vendorDir . '/verbb/super-table/src',
+    ),
+    'name' => 'Super Table',
+    'version' => '2.3.0',
+    'description' => 'Super-charge your Craft workflow with Super Table. Use it to group fields together or build complex Matrix-in-Matrix solutions.',
+    'developer' => 'Verbb',
+    'developerUrl' => 'https://verbb.io',
+    'developerEmail' => 'support@verbb.io',
+    'documentationUrl' => 'https://github.com/verbb/super-table',
+    'changelogUrl' => 'https://raw.githubusercontent.com/verbb/super-table/craft-3/CHANGELOG.md',
+  ),
+);
